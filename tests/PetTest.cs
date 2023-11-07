8adf6073be5f9a2e8c48cd99fb17950389182c5f
Marilsa-Barros <marilsa_barros@hotmail.com>
2023-11-07 16:01:45 -0300

    Teste de API utilizando csv

--- tests/PetTest.cs
+++ tests/PetTest.cs
@@ -12,10 +12,30 @@ public class PetTest
     // 3.1 - Atributos
     // Endereço da API
     private const string BASE_URL = "https://petstore.swagger.io/v2/";
-
-    //public String token; // sseria uma forma de fazer
+    //public String token; // seria uma forma de fazer
 
     // 3.2 - Funções e Métodos
+
+    // Funcção de leitura de dados a partir de um arquivo csv
+ public static IEnumerable<TestCaseData> getTestData()
+ {
+    String caminhoMassa = @"C:\Iterasys\PetStore139\fixtures\pets.csv";
+
+    using var reader = new StreamReader(caminhoMassa);
+    
+    // Pula a primeira linha com os cabeçalhos
+    reader.ReadLine();
+
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        var values = line.Split(",");
+
+        yield return new TestCaseData(int.Parse(values[0]), int.Parse(values[1]), values[2], values[3], values[4], values[5], values[6], values[7]);
+    }
+
+}
+
     [Test, Order(1)]
     public void PostPetTest()
     {
@@ -122,8 +142,12 @@ public class PetTest
         Console.WriteLine(responseBody);
 
         Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        Assert.That((int)responseBody.id, Is.EqualTo(petModel.id));
+        Assert.That((String)responseBody.tags[1].name, Is.EqualTo(petModel.tags[1].name));
+        Assert.That((String)responseBody.status, Is.EqualTo(petModel.status));
     }
-  [Test, Order(4)]
+
+    [Test, Order(4)]
     public void DeletePetTest()
     {
         // Configura
@@ -141,8 +165,85 @@ public class PetTest
         Assert.That((int)response.StatusCode, Is.EqualTo(200));
         Assert.That((int)responseBody.code, Is.EqualTo(200));
         Assert.That((String)responseBody.message, Is.EqualTo(petId));
-    }    
+    }   
 
+    [TestCaseSource("getTestData", new object[]{}), Order(5)]
+    public void PostPetDDTest(int petId,
+                              int categoryId,
+                              String categoryName,
+                              String petName,
+                              String photoUrls,
+                              String tagsIds,
+                              String tagsName,
+                              String status
+                              )
+        
+    {
+        // Configura
+         PetModel petModel = new PetModel();
+        petModel.id = petId;
+        petModel.category = new Category(categoryId, categoryName);
+        petModel.name = petName;
+        petModel.photoUrls = new String[]{photoUrls};
+
+        //Códgo para gerar as multiplas tags que o pet pode ter
+        String[] tagsIdsList = tagsIds.Split(";");  // Ler
+        String[] tagsNameList = tagsName.Split(";"); // Ler
+        List<Tag> tagList = new List<Tag>(); // Gravar depois do for
+
+        for (int i = 0; i < tagsIdsList.Length; i++)
+        {
+            int tagId = int.Parse(tagsIdsList[i]);
+            String tagName = tagsNameList[i];
+
+            Tag tag = new Tag(tagId, tagName);
+            tagList.Add(tag);
+        }
+
+        petModel.tags = tagList.ToArray();
+
+        petModel.status = status;
+
+        // A estrutura de dados está pronta, agora vamos serializar
+        String jsonBody = JsonConvert.SerializeObject(petModel, Formatting.Indented);
+        Console.WriteLine(jsonBody);
+
+
+        // instancia o objeto do tipo RestClient com o endereço da API
+        var client = new RestClient(BASE_URL);
+
+        // instancia o objeto do tipo RestRequest com o complemento de endereço
+        // como "pet" e configurando o método para ser um post (inclusão)
+        var request = new RestRequest("pet", Method.Post);
+        
+        // adiciona na requisição o conteúdo do arquivo pet1.json
+        request.AddBody(jsonBody);
+
+        // Executa
+        // executa a requisição conforme a configuração realizada
+        // guarda o json retornado no objeto response
+        var response = client.Execute(request);
+
+        // Valida
+        var responseBody = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+        // Exibe o responseBody no console
+        Console.WriteLine(responseBody);
+
+        // Valide que na resposta, o status code é igual ao resultado esperado (200)
+        Assert.That((int)response.StatusCode, Is.EqualTo(200));
+
+        // Valida o petId
+        //int petId = responseBody.id;
+        Assert.That((int)responseBody.id, Is.EqualTo(petId));
+        
+        // Valida o nome do animal na resposta
+        Assert.That((String)responseBody.name, Is.EqualTo(petName));
+ 
+        Assert.That((String)responseBody.status, Is.EqualTo(status));
+        
+    }
+ 
     [Test, Order(6)]    
     public void GetUserLoginTest()
     {
@@ -163,7 +264,7 @@ public class PetTest
         Assert.That((int)responseBody.code, Is.EqualTo(200));
         String message = responseBody.message;
         String token = message.Substring(message.LastIndexOf(":")+1);
-        Console.WriteLine($"token = {token}");
+        Console.WriteLine($"Token = {token}");
         Environment.SetEnvironmentVariable("token", token);
 
     }
